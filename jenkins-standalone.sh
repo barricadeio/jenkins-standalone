b778d375de22912760fdaa7161e773e854778d74
Pradeep Chhetri <pradeep@barricade.io>
2016-03-29 17:22:52 +0100

    Jenkin 2.0 Testing

--- jenkins-standalone.sh
+++ jenkins-standalone.sh
@@ -147,5 +147,4 @@ java \
     --ajp13Port=-1                                 \
     --httpListenAddress=0.0.0.0                    \
     --ajp13ListenAddress=127.0.0.1                 \
-    --preferredClassLoader=java.net.URLClassLoader \
     --logfile=../jenkins.log
